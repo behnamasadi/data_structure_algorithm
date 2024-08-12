38cb466399eb4d59e95689953549ccca0eab47e0
Behnam Asadi <behnam.asadi@gmail.com>
2024-08-12 10:52:36 +0200

    practice

--- src/array/main.cpp
+++ src/array/main.cpp
@@ -1,51 +1,85 @@
+
+
+#include <algorithm>
+#include <array>
 #include <iostream>
 
-template
-<typename T>
-class Array 
-{
-public:
-    //T* data;
-    T data[50];
-    std::size_t  size, lenght;
-    
-    Array() {}
+template <typename T> void printArray(T arr[], std::size_t size) {
+  for (std::size_t i = 0; i < size; i++)
+    std::cout << arr[i] << " ";
+  std::cout << std::endl;
+}
 
-    int binarySearch( T key, std::size_t low, std::size_t high)
-    {
-        while (low < high)
-        {
-            std::size_t mid = (low + high) / 2;
-            if (key == data[mid])
-            {
-                return mid;
-            }
-            else if (key < data[mid])
-            {
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
-            }
-        }
-        return -1;
+void arrayTwoPointer() {
+
+  std::array<int, 6> arr = {10, 20, 35, 50, 75, 80};
+  int val = 70;
+  auto begin = arr.begin();
+  auto end = arr.end();
+  int rest;
+  bool found = false;
+  while (found) {
+    rest = val - *end;
+    if (rest < 0)
+      end--;
+    if (rest + *begin < rest) {
     }
-    bool isSorted()
-    {
-        for (std::size_t i = 0; i < lenght; i++) 
-        {
-            if (data[i] > data[i + 1])
-                return false;
-        }
-        return true;
-    }
+  }
+}
 
+int main() {
+  // Array<int> arr = { {2,34,7,8,45},20,5 };
+  // int* p = {2,34,7,8,45 };
 
+  std::array<int, 5> arr = {2, 34, 7, 8, 45};
 
-};
-int main()
-{
-    //Array<int> arr = { {2,34,7,8,45},20,5 };
-    //int* p = {2,34,7,8,45 };
+  int n = 34;
+  // std::binary_search()
+  auto ix = std::find(arr.begin(), arr.end(), n);
+  std::cout << *std::next(ix) << std::endl;
+
+  std::cout << std::distance(arr.begin(), ix) << std::endl;
+
+  std::sort(arr.begin(), arr.end(), std::greater{});
+
+  std::cout << std::boolalpha
+            << std::is_sorted(arr.begin(), arr.end(), std::greater{})
+            << std::endl;
+
+  // std::search
+  // std::find_if
+  // std::find_first_of
+  //
+
+  std::string s1, s2;
+
+  s1 = "kuewgfwiefgywemnhyuewetiriwoedcebcoweofce";
+  s2 = "ue";
+
+  auto idx = std::search(s1.begin(), s1.end(),
+                         std::boyer_moore_searcher(s2.begin(), s2.end()));
+
+  std::cout << idx - s1.begin() << std::endl;
+
+  //   const auto it =      std::search(haystack.begin(), haystack.end(),
+  //   std::boyer_moore_searcher(needle.begin(), needle.end()));
+
+  printArray(&s1[0], s1.size());
+
+  std::nth_element(s1.begin(), s1.begin() + 2, s1.end(), std::less{});
+
+  std::cout << s1[2] << std::endl;
+
+  std::sort(s1.begin(), s1.end(), std::less{});
+
+  printArray(&s1[0], s1.size());
+
+  printArray(&arr[0], 5);
+
+  auto first_even = std::find_if(
+      arr.begin(), arr.end(), [](auto element) { return (element % 2 == 0); });
+
+  std::cout << *first_even << std::endl;
+
+  // std::stransform
 }
