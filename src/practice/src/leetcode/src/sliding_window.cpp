b142f713719b3fd34cabd0f8750ba667bd0852ac
Behnam Asadi <behnam.asadi@gmail.com>
2024-09-03 09:51:32 +0200

    sliding window lengthOfLongestSubstring added

--- src/practice/src/leetcode/src/sliding_window.cpp
+++ src/practice/src/leetcode/src/sliding_window.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <vector>
 using namespace std;
 
@@ -13,6 +14,19 @@ https://www.youtube.com/watch?v=aYqYMIqZx5s
 class Solution209 {
 public:
   int minSubArrayLen(int target, vector<int> &nums) {}
+};
+
+/*
+
+Longest Substring Without Repeating Characters
+
+https://leetcode.com/problems/longest-substring-without-repeating-characters/?envType=study-plan-v2&envId=top-interview-150
+https://www.youtube.com/watch?v=wiGpQwVHdE0
+*/
+
+class Solution3 {
+public:
+  int lengthOfLongestSubstring(string s) {}
 };
 
 int main() { return 0; }
